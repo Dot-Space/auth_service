c1307801cc90257cc46f57d0a45d86388a1126a0
Swesdek <zeroxeno@mail.ru>
2025-01-15 16:05:32 +0500

    Added db queries

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -2,8 +2,11 @@ package db
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 
+	"github.com/Dot-Space/auth_service/internal/models"
 	"github.com/lib/pq"
 )
 
@@ -18,7 +21,9 @@ func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (
 	res, err := stmt.ExecContext(ctx, email, passHash)
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
-
+			if pqErr.Code.Name() == "unique_violation" {
+				return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+			}
 		}
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -30,4 +35,27 @@ func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (
 	}
 
 	return id, nil
+}
+
+func (s *Storage) GetUser(ctx context.Context, email string) (models.User, error) {
+	const op = "db.GetUser"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash FROM users WHERE email = ?")
+	if err != nil {
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowContext(ctx, email)
+
+	user := models.User{}
+	err = row.Scan(&user.ID, &user.Email, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, ErrUserNotFound)
+		}
+
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
 }
